2b037dbf2ec9028877210587fde88143ade3f280
Tyler Brooks <thetylerbrooks@gmail.com>
2022-06-03 12:52:46 -0600

    Entry data is being passed and checked for Mnger/engnr/intern type

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Manager = require("./manager.js");
+const Engineer = require("./engineer.js");
+const Intern = require("./inter.js");
 
 const managerQs = [
   {
@@ -86,3 +89,43 @@ const internQs = [
     choices: ["Engineer", "Intern", "Finished with entries"],
   },
 ];
+
+let cardObjs = [];
+
+//called when create Engineer or Intern or Finish is selected
+function addOrFinish(data) {
+  //determine wether Manager/Engineer/Intern and then call correct class constructor
+  if (data.officeNum) {
+    console.log("Manager");
+  }
+  if (data.github) {
+    console.log("Engineer");
+  }
+  if (data.school) {
+    console.log("Intern");
+  }
+  //bring up next question block or quit based on add Engineer/Intern or Finished with entries
+  if (data.addOrFinish === "Engineer") {
+    inquirer.prompt(engineerQs).then((data) => {
+      addOrFinish(data);
+    });
+  }
+  if (data.addOrFinish === "Intern") {
+    inquirer.prompt(internQs).then((data) => {
+      addOrFinish(data);
+    });
+  }
+  if (data.addOrFinish === "Finished with entries") {
+    // inquirer.prompt(engineerQs).then((data) => {
+    //     console.log(data)
+    // })
+  }
+}
+
+function init() {
+  inquirer.prompt(managerQs).then((data) => {
+    addOrFinish(data);
+  });
+}
+
+init();
